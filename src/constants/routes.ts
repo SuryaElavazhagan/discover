2e47659035f6d7e1ca7ff68cbd09776aa2b5d746
Surya Elavazhagan <surya.elash98@protonmail.com>
2021-08-28 14:31:13 +0530

    added constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+const NAVBAR_ITEMS: Record<string, string> = {
+  popular: 'Popular',
+  trend: 'Trend',
+  new: 'Newest',
+  top: 'Top rated'
+}
+
+export {
+  NAVBAR_ITEMS
+};
---
+++ src/constants/tmdb.ts
@@ -0,0 +1,9 @@
+const TMDB = {
+  BASE_URL: 'https://api.themoviedb.org/3',
+  IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/original',
+  APIKEY: process.env.REACT_APP_TMDB_API_KEY
+};
+
+export {
+  TMDB
+};
