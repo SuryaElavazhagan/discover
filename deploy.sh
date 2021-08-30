f971c5873cdca481f483ee85850af61ad375517d
Surya Elavazhagan <surya.elash98@protonmail.com>
2021-08-30 15:03:47 +0530

    commented out aws s3 bucket push

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 yarn build
 
 # Upload files to AWS S3 bucket
-aws s3 cp build "s3://clumio-discover" --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=9e202f33659a5bb049f05c298af497560a88036746a2e2b2f03a2d790973d1b7 --region=us-east-2
+# aws s3 cp build "s3://clumio-discover" --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=9e202f33659a5bb049f05c298af497560a88036746a2e2b2f03a2d790973d1b7 --region=us-east-2
 
 # Push the html file to surge
 surge build https://tmdb-discover.surge.sh
